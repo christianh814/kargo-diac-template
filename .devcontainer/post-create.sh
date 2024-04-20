0484418e653c57076a106ca9e36d8c35d2775ebf
Christian Hernandez <christian@chernand.io>
2024-04-19 17:24:03 -0700

    Added more logic in the Dockerfile
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,23 +6,6 @@ echo "post-create start" >> ~/.status.log
 ## Install the things
 curl -L https://raw.githubusercontent.com/akuity/kargo/main/hack/quickstart/kind.sh | bash
 
-## Set up some nice bash completion
-
-cat <<EOC >> /home/vscode/.bashrc
-source <(kubectl completion bash)
-source <(akuity completion bash)
-source <(argocd completion bash)
-source <(kustomize completion bash)
-source <(kind completion bash)
-source <(kargo completion bash)
-source <(helm completion bash)
-alias k="kubectl"
-complete -F __start_kubectl k
-EOC
-
-chown $(id -u):$(id -g) /home/vscode/.bashrc
-chmod 0644 /home/vscode/.bashrc
-
 ## Update Repo With proper username
 bash .devcontainer/update-repo-for-workshop.sh
 
