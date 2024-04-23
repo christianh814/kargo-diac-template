9249f191b979ef1d2af92bc13a7a4bd1f213b830
Christian Hernandez <christian@chernand.io>
2024-04-22 18:54:51 -0700

    tracking freight to be verified
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -32,12 +32,20 @@ do
 done
 
 ## Preseed Freight by promoting it
+freight=$(kargo get freight --project kargo-demo -o jsonpath='{.metadata.name}')
 for stage in test uat prod
 do
-	freight=$(kargo get freight --project kargo-demo -o jsonpath='{.metadata.name}')
 	promotion=$(kargo promote --project kargo-demo --freight ${freight} --stage ${stage} -o jsonpath='{.metadata.name}')
 	kubectl wait --for jsonpath='{.status.phase}'=Succeeded promotions.kargo.akuity.io ${promotion} -n kargo-demo --timeout=60s
 	kubectl wait --for jsonpath='{.status.currentFreight.name}'=${freight} stages.kargo.akuity.io ${stage} -n kargo-demo --timeout=60s
+	## Until we get --wait in promotion we have to do the following: Track https://github.com/akuity/kargo/issues/1888
+	promotioncounter=0
+	until [[ $(kubectl get freights.kargo.akuity.io -n kargo-demo ${freight} -o jsonpath='{.status.verifiedIn}' | grep -c ${stage}) -ne 0 ]]
+	do
+		[[ ${promotioncounter} -gt 6 ]] && echo "Promotion took too long to verify" && exit 13
+		echo "waiting for promotion to be verified"
+		promotioncounter=$((promotioncounter+1))
+	done
 done
 
 
